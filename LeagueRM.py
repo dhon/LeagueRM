39e53ce7859606d8b624b50bf787b87c7a3a94a9
dhon <morgangrimes14@gmail.com>
2017-01-19 22:02:27 -0600

    moved TryExp into methods, better error msgs

--- LeagueRM.py
+++ LeagueRM.py
@@ -2,48 +2,79 @@
 import requests # allows easy use of JSONs from API
 import sys # allows closing of program in try/except
 
-def requestSummonerData(region, summonerName, APIKey):
+# summoner-v1.4
+def requestSummonerID(region, summonerName, APIKey):
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v1.4/summoner/by-name/" + summonerName + "?api_key=" + APIKey
-    response = requests.get(URL) # Gets the JSON File
-    return response.json() # Return JSON File
+    JSON = requests.get(URL).json() # Gets the JSON File
 
-def requestChampionData(championName, APIKey):
+    try:
+        SID = JSON[summonerName]['id']
+        SID = str(SID)
+    except:
+        print("Invalid summoner name.")
+        sys.exit(1)
+    return SID
+
+# lol-static-data-v1.2
+def requestChampionID(championName, APIKey):
     URL = "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=" + APIKey
-    response = requests.get(URL) # Gets the JSON File
-    return response.json() # Return JSON File
+    JSON = requests.get(URL).json() # Gets the JSON File
 
-def requestChampionHistory(region, ID, CID, APIKey):
-    URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.2/matchlist/by-summoner/" + ID + "?championIds=" + CID + "&rankedQueues=TEAM_BUILDER_RANKED_SOLO&beginIndex=0&endIndex=5&api_key=" + APIKey
-    response = requests.get(URL) # Gets the JSON File
-    return response.json() # Return JSON File
+    try:
+        CID = JSON['data'][championName]['id']
+        CID = str(CID)
+    except:
+        print("Invalid champion name.")
+        sys.exit(1)
+    return CID # Return JSON File
 
+# matchlist-v2.2
+def requestMatchID(region, SID, CID, APIKey):
+    URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.2/matchlist/by-summoner/" + SID + "?championIds=" + CID + "&rankedQueues=TEAM_BUILDER_RANKED_SOLO&beginIndex=0&endIndex=5&api_key=" + APIKey
+    JSON = requests.get(URL).json() # Gets the JSON File
+
+    try:
+        MID = []
+        for i in range(5):
+            temp = JSON['matches'][i]['matchId']
+            temp = str(temp)
+            MID.append(temp)
+    except:
+        print("Summoner has not played five 2017 ranked solo games on this champion.")
+        sys.exit(1)
+    return MID # Return JSON File
+
+# match-v2.2
 def requestGameMatch(region, MID, APIKey):
     URL = "https://" + region + ".api.pvp.net/api/lol/" + region + "/v2.2/match/" + MID + "?api_key=" + APIKey
-    response = requests.get(URL) # Gets the JSON File
-    return response.json() # Return JSON File
+    JSON = requests.get(URL).json() # Gets the JSON File
 
+    try:
+        test = JSON['participantIdentities'][0]['player']['summonerId']
+        test = str(test)
+    except:
+        print("No API Calls remaining. Try again later.")
+        sys.exit(1)
+    return JSON # Return JSON File
+
+# lol-static-data-v1.2
 def requestRuneData(runeId, APIKey):
     URL = 'https://global.api.pvp.net/api/lol/static-data/na/v1.2/rune/' + runeId + '?api_key=' + APIKey
-    response = requests.get(URL) # Gets the JSON File
-    json = response.json() # Stores JSON File
-    return str(json['name']) # Return Rune Name
+    JSON = requests.get(URL).json() # Gets the JSON File
+    return str(JSON['name']) # Return Rune Name
 
-def getParticipantIdentity(MatchJSON, ID):
-    try:
-        for i in range(10):
-            if(MatchJSON['participantIdentities'][i]['player']['summonerId'] == int(ID)):
-                PID = MatchJSON['participantIdentities'][i]['participantId']
-    except:
-        print("No Riot API Calls remaining. Try again later.")
-        sys.exit(1)
+def getParticipantIdentity(MatchJSON, SID):
+    for i in range(10):
+        if(MatchJSON['participantIdentities'][i]['player']['summonerId'] == int(SID)):
+            PID = MatchJSON['participantIdentities'][i]['participantId']
     return PID
 
-def getRunes(MatchJSON, ID, PID, APIKey):
+def getRunes(MatchJSON, PID, APIKey):
     for i in range(len(MatchJSON['participants'][PID-1]['runes'])):
         rank = str(MatchJSON['participants'][PID-1]['runes'][i]['rank'])
         runeId = str(MatchJSON['participants'][PID-1]['runes'][i]['runeId'])
         runeName = requestRuneData(runeId, APIKey)
-        print(rank + 'x' + ' ' + runeName)
+        print(rank + 'x ' + runeName)
     return
     
 def main():
@@ -56,69 +87,37 @@ def main():
     print("JarvanIV, Khazix, KogMaw, Leblanc, LeeSin, MasterYi, MissFortune, MonkeyKing,")
     print("RekSai, TahmKench, TwistedFate, Velkoz, XinZhao.\n")
 
-    # Inputs
+    # Input
     region = (str)(input('Region: ')).lower()
     summonerName = (str)(input('Summoner Name: ')).lower()
     championName = (str)(input('Champion Name: '))
     APIKey = (str)(input('Type in your API Key here: '))
 
-    # Look up their Summoner ID.
-    try:
-        responseJSON = requestSummonerData(region, summonerName, APIKey)
-    except:
-        print("Invalid Region or APIKey.")
-        sys.exit(1)
-    
-    # Save their Summoner ID.
-    try:
-        ID = responseJSON[summonerName]['id']
-        ID = str(ID)
-    except:
-        print("Invalid summoner name.")
-        sys.exit(1)
+    # Look up Summoner ID
+    SID = requestSummonerID(region, summonerName, APIKey)
 
-    # Look up list of Champions and IDs.
-    responseJSON2 = requestChampionData(championName, APIKey)
-
-    # Translate Champion Name to Champion ID.
-    try:
-        CID = responseJSON2['data'][championName]['id']
-        CID = str(CID)
-    except:
-        print("Invalid champion name.")
-        sys.exit(1)
+    # Look up Champion ID
+    CID = requestChampionID(championName, APIKey)
 
     # Look for 5 matches of that champion
-    responseJSON3 = requestChampionHistory(region, ID, CID, APIKey)
+    MID = requestMatchID(region, SID, CID, APIKey)
 
-    # Save the 5 match history IDs
-    try:
-        MID = []
-        for i in range(5):
-            temp = responseJSON3['matches'][i]['matchId']
-            temp = str(temp)
-            MID.append(temp)
-    except:
-        print("Either Summoner has not played 5 games of this champion in 2017 solo ranked,")
-        print("OR Riot APIKey has ran out of calls early on in the program: Try again.")
-        sys.exit(1)
-
-    # Save the 5 match history JSONs
+    # Save the 5 match JSONs
     MatchJSON = []
     for i in range(5):
         temp = requestGameMatch(region, MID[i], APIKey)
         MatchJSON.append(temp)
 
-    # Save the 5 participant IDs from matches
+    # Save the 5 participant IDs
     PID = []
     for i in range(5):
-        temp = getParticipantIdentity(MatchJSON[i], ID)
+        temp = getParticipantIdentity(MatchJSON[i], SID)
         PID.append(temp)
 
-    # Print the runes used in the 5 matches
+    # Print the runes used in the matches
     for i in range(5):
         print('Match ' + str(i+1) + ': ')
-        getRunes(MatchJSON[i], ID, PID[i], APIKey)
+        getRunes(MatchJSON[i], PID[i], APIKey)
 
 # Start
 main()
