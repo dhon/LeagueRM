70b912e681507b92d101b7ba7c9f4294aca6563e
dhon <morgangrimes14@gmail.com>
2017-01-25 12:49:39 -0600

    working on SQLite

--- Test.py
+++ Test.py
@@ -1,10 +1,13 @@
 import sqlite3
+import time
+import datetime
+import random
 
 conn = sqlite3.connect('tutorial.db')
 c = conn.cursor()
 
 def create_table():
-	c.execute('CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)')
+	c.execute("CREATE TABLE IF NOT EXISTS stuffToPlot(unix REAL, datestamp TEXT, keyword TEXT, value REAL)")
 
 def data_entry():
 	c.execute("INSERT INTO stuffToPlot VALUES(145123542, '2016-01-01', 'Python', 5)")
@@ -12,5 +15,19 @@ def data_entry():
 	c.close()
 	conn.close()
 
+def dynamic_data_entry():
+	unix = time.time()
+	date = str(datetime.datetime.fromtimestamp(unix).strftime('%Y-%m-%d %H:%M:%S'))
+	keyword = 'Python'
+	value = random.randrange(0,10)
+	c.execute("INSERT INTO stuffToPlot (unix, datestamp, keyword, value) VALUES (?, ?, ?, ?)",
+		(unix, date, keyword, value))
+	conn.commit()
+
 create_table()
-data_entry()
+# data_entry()
+for i in range(10):
+	dynamic_data_entry()
+	time.sleep(1)
+c.close
+conn.close
